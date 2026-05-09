8f7aa17a27bb5a6685c63b312d63452ddc86eca0
Ygolana <villdreg@gmail.com>
2026-05-09 16:42:10 +0800

    Add files via upload

---
+++ suckless/slock/config.h
@@ -0,0 +1,21 @@
+/* user and group to drop privileges to */
+static const char *user  = "flrou";
+static const char *group = "wheel";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#202020",   /* during input */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* default message */
+static const char * message = "LOCKED. PASSWORD REQUIRED. PLEASE BE GENTLE WITH THE KEYS.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "10x20";
