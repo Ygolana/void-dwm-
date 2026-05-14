a3a6c2965e16cec8d77a3cb298fa3b4d68b3763e
Ygolana <villdreg@gmail.com>
2026-05-14 19:26:45 +0800

    Update config.def.h

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -149,7 +149,7 @@ static const char *toggle[]         = { "mpc", "toggle", NULL };
 static const char *next[]           = { "mpc", "next", NULL };
 static const char *prev[]           = { "mpc", "prev", NULL };
 static const char *brighter[]	    = { "brightnessctl", "set", "10%+", NULL };
-static const char *dimmer[]	    = { "brightnessctl", "set", "10%-", NULL };
+static const char *dimmer[]			= { "brightnessctl", "set", "10%-", NULL };
 
 static const Key keys[] = {
 	/* modifier                      key                function               argument */
@@ -195,14 +195,14 @@ static const Key keys[] = {
 	{MODKEY|ShiftMask,               XK_c,            quit,                   {0} },
 
 
-	{0,	       XF86XK_AudioRaiseVolume,		  spawn,		  SHCMD("amixer -c 1 set Master 5%+ unmute") },
-	{0,	       XF86XK_AudioLowerVolume,		  spawn,		  SHCMD("amixer -c 1 set Master 5%- unmute") },
-	{0,		      XF86XK_AudioMute,		  spawn,		  SHCMD("amixer -c 1 set Master toggle") },
-	{0,                              XK_F6,           spawn,                  {.v = toggle}},
-	{0,                              XK_F8,           spawn,                  {.v = next}},
-	{0,                              XK_F7,           spawn,                  {.v = prev}},
-	{0, 	      XF86XK_MonBrightnessDown,		  spawn,		  {.v = dimmer } },
-	{0,		XF86XK_MonBrightnessUp,		  spawn,		  {.v = brighter } },
+	{0,			  XF86XK_AudioRaiseVolume,			  spawn,				  SHCMD("amixer -c 1 set Master 5%+ unmute") },
+	{0,			  XF86XK_AudioLowerVolume,			  spawn,				  SHCMD("amixer -c 1 set Master 5%- unmute") },
+	{0,					 XF86XK_AudioMute,			  spawn,				  SHCMD("amixer -c 1 set Master toggle") },
+	{0,                             XK_F6,			  spawn,                  {.v = toggle}},
+	{0,                             XK_F8,			  spawn,                  {.v = next}},
+	{0,                             XK_F7,			  spawn,                  {.v = prev}},
+	{0,			 XF86XK_MonBrightnessDown,			  spawn,				  {.v = dimmer } },
+	{0,			   XF86XK_MonBrightnessUp,			  spawn,				  {.v = brighter } },
 
 		TAGKEYS(                         XK_1,                            0)
 		TAGKEYS(                         XK_2,                            1)
